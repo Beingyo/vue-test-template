7ad5a540782ee9e95f77718354d23e7ea75af633
Beingyo <58251806+Beingyo@users.noreply.github.com>
2021-02-24 09:29:53 +0800

    点击更换样式

---
+++ src/page/clickChangeStyle/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul>
+    <li
+      v-for="(item,index) in list"
+      :key="index"
+      class="normal-class"
+      :class="{ 'active-class': nowIndex === index }"
+      @click="changeClass(index)"
+    >
+      点我
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'clickChangeStyle',
+    data () {
+      return {
+        list:[1, 2, 3, 4, 5],
+        nowIndex: 0
+      }
+    },
+    methods: {
+      changeClass (index) {
+        this.nowIndex = index
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .normal-class{
+    display: inline-block;
+    width: 30px;
+    margin: 10px;
+    background: lightgray;
+  }
+  .active-class{
+    background: darkgray;
+  }
+</style>
