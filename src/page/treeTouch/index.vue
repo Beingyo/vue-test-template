5d3124ecd672e1efc4ac8f2650e2a0feaf03b29f
Beingyo <58251806+Beingyo@users.noreply.github.com>
2021-02-24 15:45:32 +0800

    elTree触摸样式

--- src/page/treeTouch/index.vue
+++ src/page/treeTouch/index.vue
@@ -16,7 +16,7 @@
               <span>{{ node.label }}</span>
               <span v-if="data.deptId === clickIndex || overIndex === data.deptId">
                 <el-popover
-                  v-model="data.visible"
+                  :v-model="data.visible = true"
                   placement="bottom"
                   width="200"
                   @hide="cancelDepartmen(data)">
@@ -32,7 +32,7 @@
                     </div>
                   </div>
                   <div v-if="type == 2 || type == 3">
-                    <el-form ref="ruleForm" :model="ruleForm" :rules="rules" >
+                    <el-form ref="ruleForm" :model="ruleForm" >
                       <el-form-item prop="deptName">
                         <el-input v-model="ruleForm.deptName"/>
                       </el-form-item>
@@ -40,7 +40,7 @@
                     <el-button @click="addsub(data)">确定</el-button>
                     <el-button @click="cancelDepartmen(data)">取消</el-button>
                   </div>
-                  <el-button slot="reference" type="text" icon="el-icon-more" @click="tools(data)"/>
+                  <el-button class="btn" slot="reference" type="text" icon="el-icon-more" @click="tools(data)"/>
                 </el-popover>
               </span>
             </span>
@@ -48,6 +48,7 @@
         </div>
       </div>
     </div>
+    <div class="right">显示内容</div>
   </div>
 </template>
 
@@ -57,12 +58,15 @@
     name: 'Employee',
     data() {
       return {
-        data: [{"deptName":"富士","deptStatus":1,"deptParentId":0,"deptId":1,"sub":[{"deptName":"部门1","deptStatus":1,"deptParentId":1,"deptId":2,"sub":[{"deptName":"部门1-2","deptStatus":1,"deptParentId":2,"deptId":7,"sub":[],"struct":"富士/部门1/部门1-2","parentName":"部门1","showName":null,"origin":"1,2,7"},{"deptName":"部门1-1","deptStatus":1,"deptParentId":2,"deptId":6,"sub":[{"deptName":"部门1-1-1","deptStatus":1,"deptParentId":6,"deptId":12,"sub":[],"struct":"富士/部门1/部门1-1/部门1-1-1","parentName":"部门1-1","showName":null,"origin":"1,2,6,12"},{"deptName":"部门1-1-2","deptStatus":1,"deptParentId":6,"deptId":13,"sub":[],"struct":"富士/部门1/部门1-1/部门1-1-2","parentName":"部门1-1","showName":null,"origin":"1,2,6,13"}],"struct":"富士/部门1/部门1-1","parentName":"部门1","showName":null,"origin":"1,2,6"},{"deptName":"部门1-3","deptStatus":1,"deptParentId":2,"deptId":8,"sub":[],"struct":"富士/部门1/部门1-3","parentName":"部门1","showName":null,"origin":"1,2,8"}],"struct":"富士/部门1","parentName":"富士","showName":null,"origin":"1,2"},{"deptName":"部门2","deptStatus":1,"deptParentId":1,"deptId":3,"sub":[{"deptName":"部门2-1","deptStatus":1,"deptParentId":3,"deptId":9,"sub":[],"struct":"富士/部门2/部门2-1","parentName":"部门2","showName":null,"origin":"1,3,9"},{"deptName":"部门2-2","deptStatus":1,"deptParentId":3,"deptId":10,"sub":[],"struct":"富士/部门2/部门2-2","parentName":"部门2","showName":null,"origin":"1,3,10"}],"struct":"富士/部门2","parentName":"富士","showName":null,"origin":"1,3"},{"deptName":"部门3","deptStatus":1,"deptParentId":1,"deptId":4,"sub":[{"deptName":"部门3-1","deptStatus":1,"deptParentId":4,"deptId":11,"sub":[],"struct":"富士/部门3/部门3-1","parentName":"部门3","showName":null,"origin":"1,4,11"}],"struct":"富士/部门3","parentName":"富士","showName":null,"origin":"1,4"},{"deptName":"部门4","deptStatus":1,"deptParentId":1,"deptId":5,"sub":[],"struct":"富士/部门4","parentName":"富士","showName":null,"origin":"1,5"}],"struct":"富士","parentName":null,"showName":"富士","origin":"1"}],
+        data: [{"deptName":"XX公司","deptStatus":1,"deptParentId":0,"deptId":1,"sub":[{"deptName":"部门1","deptStatus":1,"deptParentId":1,"deptId":2,"sub":[{"deptName":"部门1-2","deptStatus":1,"deptParentId":2,"deptId":7,"sub":[],"struct":"富士/部门1/部门1-2","parentName":"部门1","showName":null,"origin":"1,2,7"},{"deptName":"部门1-1","deptStatus":1,"deptParentId":2,"deptId":6,"sub":[{"deptName":"部门1-1-1","deptStatus":1,"deptParentId":6,"deptId":12,"sub":[],"struct":"富士/部门1/部门1-1/部门1-1-1","parentName":"部门1-1","showName":null,"origin":"1,2,6,12"},{"deptName":"部门1-1-2","deptStatus":1,"deptParentId":6,"deptId":13,"sub":[],"struct":"富士/部门1/部门1-1/部门1-1-2","parentName":"部门1-1","showName":null,"origin":"1,2,6,13"}],"struct":"富士/部门1/部门1-1","parentName":"部门1","showName":null,"origin":"1,2,6"},{"deptName":"部门1-3","deptStatus":1,"deptParentId":2,"deptId":8,"sub":[],"struct":"富士/部门1/部门1-3","parentName":"部门1","showName":null,"origin":"1,2,8"}],"struct":"富士/部门1","parentName":"富士","showName":null,"origin":"1,2"},{"deptName":"部门2","deptStatus":1,"deptParentId":1,"deptId":3,"sub":[{"deptName":"部门2-1","deptStatus":1,"deptParentId":3,"deptId":9,"sub":[],"struct":"富士/部门2/部门2-1","parentName":"部门2","showName":null,"origin":"1,3,9"},{"deptName":"部门2-2","deptStatus":1,"deptParentId":3,"deptId":10,"sub":[],"struct":"富士/部门2/部门2-2","parentName":"部门2","showName":null,"origin":"1,3,10"}],"struct":"富士/部门2","parentName":"富士","showName":null,"origin":"1,3"},{"deptName":"部门3","deptStatus":1,"deptParentId":1,"deptId":4,"sub":[{"deptName":"部门3-1","deptStatus":1,"deptParentId":4,"deptId":11,"sub":[],"struct":"富士/部门3/部门3-1","parentName":"部门3","showName":null,"origin":"1,4,11"}],"struct":"富士/部门3","parentName":"富士","showName":null,"origin":"1,4"},{"deptName":"部门4","deptStatus":1,"deptParentId":1,"deptId":5,"sub":[],"struct":"富士/部门4","parentName":"富士","showName":null,"origin":"1,5"}],"struct":"富士","parentName":null,"showName":"富士","origin":"1"}],
         // 左侧成员树
         defaultProps: { children: 'sub', label: 'deptName' },
         type: '1', // 1 显示点击左侧小点弹出的表单， 2 显示修改名称时弹出的表单
         clickIndex: '-1', // 控制点击左侧出现圆点按钮
         overIndex: '-1', // 控制触摸左侧出现圆点按钮
+        ruleForm: {
+          deptName: '',
+        }
       }
     },
     created() {
@@ -83,7 +87,7 @@
       // 点击圆点按钮
       tools(item) {
         item.visible = true
-        // this.overIndex = data.deptId
+        // this.overIndex = item.deptId
       },
       // 左侧取消按钮
       cancelDepartmen(data) {
@@ -92,6 +96,7 @@
       },
       // 添加部门
       addsub(item) {
+        console.log('添加部门')
       },
       // 修改部门名
       editDepartmentName(item) {
@@ -102,6 +107,10 @@
       addDepartmentName(item) {
         this.type = 3
         this.ruleForm.deptName = ''
+      },
+      // 删除部门
+      deleteDepartmentName(item) {
+        console.log('删除部门')
       }
     }
   }
@@ -120,7 +129,8 @@
     flex-direction: row;
     background-color: #efefef;
     .left{
-      height: 800px;
+      width: 200px;
+      height: 500px;
       white-space:nowrap;
       overflow-y:auto;
       overflow-x:auto;
@@ -129,12 +139,19 @@
         padding: 20px;
         min-width: max-content;
         .custom-tree-node{
-          flex: 1;
+          //flex: 1;
           /*display: flex;*/
           /*align-items: center;*/
           justify-content: space-between;
         }
       }
     }
+    .right {
+      margin: 0 auto;
+      line-height: 500px;
+    }
+  }
+  .btn {
+    margin-top: 3px;
   }
 </style>
