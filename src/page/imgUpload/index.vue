2a5b529107369f46e874fe2a6fb0b5481ac4c6d8
Beingyo <58251806+Beingyo@users.noreply.github.com>
2021-02-24 09:34:28 +0800

    图片上传并显示

---
+++ src/page/imgUpload/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="margin: 0 auto;text-align: center">
+    <input type="file" @change="displayImage" ref="fileInput">
+    <img :src="previewImageSrc">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'imgUpload',
+    data () {
+      return {
+        previewImageSrc: ''
+      }
+    },
+    methods: {
+      displayImage () {
+        const file = this.$refs.fileInput.files[0]
+        const fr = new FileReader()
+        fr.onload = (e) => {
+          this.previewImageSrc = e.target.result
+        }
+        fr.readAsDataURL(file)
+      }
+    }
+  }
+</script>
