eb0e7ea147fc4b848d26ebe86eca181950b5316b
Beingyo <58251806+Beingyo@users.noreply.github.com>
2021-03-09 15:58:43 +0800

    vue-extend构造器

---
+++ src/page/vueExtend/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="content">
+    <button @click="clickBtn">点击构造</button>
+    <div id="creatExtend">
+    </div>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  export default {
+    name: 'vueExtend',
+    data () {
+      return {
+        msg: '信息：'
+      }
+    },
+    methods: {
+      clickBtn() {
+        var that = this
+        // 创建构造器
+        var Profile = Vue.extend({
+          template: '<p>{{msg}} {{firstName}} {{lastName}} aka {{alias}}</p>',
+          data() {
+            return {
+              firstName: 'Walter',
+              lastName: 'White',
+              alias: 'Heisenberg',
+              msg: that.msg
+            }
+          }
+        })
+// 创建 Profile 实例，并挂载到一个元素上。
+        new Profile().$mount('#creatExtend')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
